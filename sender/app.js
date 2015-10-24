f1b8ad1ed0e337363e2229009478ebcbbaa0072d
Nathaniel Freeman <volmasoft@gmail.com>
2015-10-24 14:20:31 +0100

    Test setup added to sender

--- sender/app.js
+++ sender/app.js
@@ -2,9 +2,28 @@ var fs = require('fs');
 var configFile = 'config.json';
 
 // Load config
+console.log("Loading configuration file(s)");
 var configuration = JSON.parse(
     fs.readFileSync(configFile)
 );
-
+console.log("...done");
 console.log("CONFIGURATION LOADED, use configuration.<key>");
 console.log(configuration);
+
+
+// Configure clockwork
+console.log("Configuring clockwork SMS");
+var clockwork = require('clockwork')({key:configuration.API_KEY});
+console.log("... done");
+
+// Test message
+clockwork.sendSms({To:configuration.TEST_PHONE_NUMBER,Content:'SPAM'}, function(error, response) {
+    if (error) {
+        console.log('Error', error);
+    } else {
+        console.log('resp==',response);
+        console.log('a',response.SMS_Resp);
+        console.log('a',response.SMS_Resp.To);
+        console.log('a',response.SMS_Resp.MessageID);
+    }
+});
